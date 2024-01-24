4cb3e1e845542b20430b4c2b4b0f4409fa210ecd
Tong Truong Thanh <69966374+Tom-Dev1@users.noreply.github.com>
2024-01-24 21:55:49 +0700

    "Create api & data for table Building and Area"

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const helmet = require("helmet");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const db = require("./src/common/connectMG");
+const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./src/common/connectMG');
 
-const tranpointRoutes = require("./src/routers/tranPoint.router");
-const transactionHistoryPointRoutes = require("./src/routers/transactionHistoryPoint.router");
-const clubRoutes = require("./src/routers/club.router");
-const clubMemberRoutes = require("./src/routers/clubMember.router");
+const tranpointRoutes = require('./src/routers/tranPoint.router');
+const transactionHistoryPointRoutes = require('./src/routers/transactionHistoryPoint.router');
+// const clubRoutes = require("./src/routers/club.router");
+// const clubMemberRoutes = require("./src/routers/clubMember.router");
 // const clubMemSlotRoutes = require("./src/routers/clubMemSlot.router");
 // const slotRoutes = require("./src/routers/slot.router");
 
-const memberRoutes = require("./src/routers/member.router");
+const memberRoutes = require('./src/routers/member.router');
 // const walletRoutes = require("./src/routers/wallet.router");
 // const areaRoutes = require("./src/routers/area.router");
 // const buildingRoutes = require("./src/routers/building.router");
@@ -30,25 +30,23 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({ secret: "your-secret-key", resave: true, saveUninitialized: true })
-);
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
 
 //Import api
-app.use("/api", memberRoutes);
-app.use("/api", transactionHistoryPointRoutes);
-app.use("/api", tranpointRoutes);
-app.use("/api", clubRoutes);
-app.use("/api", clubMemberRoutes);
+app.use('/api', memberRoutes);
+app.use('/api', transactionHistoryPointRoutes);
+app.use('/api', tranpointRoutes);
+// app.use("/api", clubRoutes);
+// app.use("/api", clubMemberRoutes);
 // app.use("/api", clubMemSlotRoutes);
 // app.use("/api", slotRoutes);
 
 // app.use("/api", authRoutes);
 // app.use("/api", memberRoutes);
 // app.use("/api", walletRoutes);
-// app.use("/api", areaRoutes);
-// app.use("/api", buildingRoutes);
-// app.use("/api", sportRoutes);
+app.use('/api', areaRoutes);
+app.use('/api', buildingRoutes);
+//app.use("/api", sportRoutes);
 // app.use("/api", yardRoutes);
 
 // app.use("/api", authMiddleware.authenticateToken, memberRoutes);
---
+++ src/models/area.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Define Area Schema
+const areaSchema = new Schema({
+  name: { type: String, required: true },
+  address: { type: String },
+  status: { type: Boolean },
+  dateTime: { type: Date },
+});
+const Area = mongoose.model('Area', areaSchema);
+module.exports = Area;
