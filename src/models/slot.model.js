a71f7f4d1de5c8a85e58aa3e72eaf36bfc96c415
Truong Thuy Truc <truc5302@gmail.com>
2024-02-22 08:58:07 +0700

    T push

--- src/models/slot.model.js
+++ src/models/slot.model.js
@@ -1,11 +1,40 @@
 const mongoose = require("mongoose");
 
 const SlotSchema = new mongoose.Schema({
-  clubMemberId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  slotId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  dateTime: { type: Date },
-  transactionPoint: { type: Number },
-  status: { type: Boolean },
+  name: {
+    type: String,
+    required: true,
+  },
+  startTime: {
+    type: Date,
+  },
+  endTime: {
+    type: Date,
+  },
+  memberPostId: {
+    type: Number,
+    required: true,
+  },
+  currentMember: {
+    type: String,
+  },
+  requiredMember: {
+    type: String,
+  },
+  yardId: {
+    type: Number,
+    required: true,
+  },
+  yardName: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+  dateTime: {
+    type: Date,
+  },
 });
 
 const Slot = mongoose.model("Slot", SlotSchema);
